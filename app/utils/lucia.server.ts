a1f3abefe4fd7c33a3b8569ff6e6bef16797b354
Damien Dumontet <damien.dumontet@protonmail.com>
2023-11-27 16:53:15 -0300

    update lucia client setup

--- app/utils/lucia.server.ts
+++ app/utils/lucia.server.ts
@@ -1,10 +1,15 @@
 import { lucia } from 'lucia';
 import { prisma } from '@lucia-auth/adapter-prisma';
 import { DatabaseClient } from './prisma.server';
+import { web } from 'lucia/middleware';
 
 export const auth = lucia({
   env: 'DEV',
   adapter: prisma(DatabaseClient),
+  middleware: web(),
+  sessionCookie: {
+    expires: false,
+  },
 });
 
 export type Auth = typeof auth;
